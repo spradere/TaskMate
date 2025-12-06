719ab89c1af34b0dcaac219a9a9794660707d49a
spradere <spradere@pm.me>
2025-12-07 00:56:47 +0100

    add sysCall/gpio and hal/hal_gpio

--- src/hal/board/arduino_mega/hal_boardInit.c
+++ src/hal/board/arduino_mega/hal_boardInit.c
@@ -16,11 +16,25 @@
  * @brief board init arduino mega implematation
  *
  */
-
+#include <avr/io.h>
+#include <stdbool.h>
 #include "hal/hal_user_api.h"
 
 void hal_boardInit()
 {
 	// Set pin output for in board led
 	IN_BOARD_LED_DDR |= (1 << IN_BOARD_LED_PIN);
+
+	// gpio pin definition
+	gpio_signal_item_t sig;
+
+	sig.pin.port = PORTB;
+	sig.pin.direction = DDRB;
+	sig.pin.number = PB7;
+	sig.pin.mode = GPIO_PIN_MODE_OUTPUT_PP;
+	sig.pin.pull = GPIO_PIN_PULL_NONE;
+	sig.active_high = true;
+
+	gpioSignalInit(GPIO_SIGNAL_INBOARD_LED, &sig);
+
 }
