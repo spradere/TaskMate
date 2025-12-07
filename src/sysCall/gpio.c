b6739d831a49f604d11db9839fc51beac272d529
spradere <spradere@pm.me>
2025-12-07 17:11:35 +0100

    change gpio port adress storage

--- src/sysCall/gpio.c
+++ src/sysCall/gpio.c
@@ -24,18 +24,23 @@ static gpio_signal_item_t gpio_signals_table[GPIO_SIGNAL_COUNT];
 
 void gpioSignalInit(gpio_signal_t signal, gpio_signal_item_t *sig)
 {
-	gpio_signals_table[signal] = (*sig);
+	gpio_signals_table[signal] = *sig;
 	hal_gpioInitPin(&(sig->pin));
 }
 
 void gpioSignalSet(gpio_signal_t signal, bool on)
 {
 	bool val = gpio_signals_table[signal].active_high ? on : !on;
-	hal_gpioWritePin(&(gpio_signals_table[signal].pin), val);
+	hal_gpioWritePin(&(gpio_signals_table[signal].pin), on);
 }
 
 bool gpioSignalGet(gpio_signal_t signal)
 {
 	bool val = hal_gpioReadPin(&(gpio_signals_table[signal].pin));
 	return gpio_signals_table[signal].active_high ? val : !val;
+}
+
+void gpioSignalToggle(gpio_signal_t signal)
+{
+	gpioSignalSet(signal, !gpioSignalGet(signal));
 }
