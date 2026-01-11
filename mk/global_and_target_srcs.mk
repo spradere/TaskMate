eb121c33d5f859d148f6ec5d87f13e7c9677929a
spradere <spradere@pm.me>
2026-01-11 18:32:27 +0100

    move autoCode sources from  utility/autoCode_src -> scr/autoCode

--- mk/global_and_target_srcs.mk
+++ mk/global_and_target_srcs.mk
@@ -47,20 +47,25 @@ DEPS_FILE = build/.deps.d
 .sinclude "${DEPS_FILE}"
 
 # autoCode
-AUTOCODE_TARGET = utility/autoCode
-AUTOCODE_SRCS != find utility/autoCode_src/ -maxdepth 1 -type f -name "*.c"
-AUTOCODE_SRCS_H != find utility/autoCode_src/ -maxdepth 1 -type f -name "*.h"
-AUTOCODE_STAMP = build/.autoCode_stamp_${ARCH}_${MCU}_${BOARD}
+AUTOCODE_TARGET = ${BUILD_DIR}/autoCode
+AUTOCODE_SRCS != find ${SRC_DIR}/autoCode/ -maxdepth 1 -type f -name "*.c"
+AUTOCODE_SRCS_H != find ${SRC_DIR}/autoCode/ -maxdepth 1 -type f -name "*.h"
+AUTOCODE_STAMP = ${BUILD_DIR}/.autoCode_stamp_${ARCH}_${MCU}_${BOARD}
 AUTOCODE_DATE_TIME != date +"%Y_%m_%d_%H:%M:%S"
 
 FILES_HAL_USER != find src/hal/ -name '*.h' -type f -exec grep -l '// @hal_user' {} +
 FILES_HAL_SYSTEM != find src/hal/ -name '*.h' -type f -exec grep -l '// @hal_system' {} +
 
+AUTOCODE_CFLAGS = -I/root/code/TaskMate/TaskMate_current/src/
+AUTOCODE_CFLAGS += -Wall -Wextra -Wshadow -Wpedantic -Wconversion \
+	-Wswitch -Wenum-conversion \
+	-Wno-gnu-zero-variadic-macro-arguments
+
 # Initrc files for autocode
 FILES_INIT_RC != find ${SRC_DIR_LIST} -maxdepth 1 -type f -name "*.rc"
 
 # glabal errors
-ERROR_ALL = build/errors_all.err
+ERROR_ALL = ${BUILD_DIR}/errors_all.err
 ERROR_FILES != find ${SRC_DIR_LIST} -name '*.err' -maxdepth 1 -type f | sort
 
 # mk file -> target help
