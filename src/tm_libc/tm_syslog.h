b60d85de2a2c40fe222cf0750d56da317fdd17a0
spradere <spradere@pm.me>
2026-02-07 21:04:59 +0100

    fix .gitignore

---
+++ src/tm_libc/tm_syslog.h
@@ -0,0 +1,38 @@
+/*
+ * TaskMate Project
+ * (c) 2025 PRADERE Sebastien
+ *
+ * This file is part of TaskMate and is distributed under the TaskMate License
+ * v1.0. See the LICENSE file for full license terms.
+ *
+ * Non-commercial use permitted under conditions. Commercial use requires a
+ * separate license. Commercial licensing inquiries:
+ * https://codeberg.org/Doul09/TaskMate/issues
+ *
+ * Powered by TaskMate, (c) 2025 PRADERE Sebastien
+ */
+
+/**
+ * @file tm_syslog.h
+ * @brief header of system log
+ *
+ */
+
+#ifndef TM_SYSLOG_H
+#define TM_SYSLOG_H
+
+#include "hal/auto_hal_define.h" // get libc selection
+
+#if TM_LIBC_CSTD
+	#include <syslog.h>
+	#define tm_syslog syslog
+#endif
+
+#if TM_LIBC_TASKMATE
+	#include <stdint.h>
+	#include <stdarg.h>
+	void tm_syslog(const char *format, ...);
+	void tm_vsyslog(const char *format, va_list args);
+#endif
+
+#endif
