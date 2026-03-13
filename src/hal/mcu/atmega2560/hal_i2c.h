eda3623986bae55b70f738fe16881408495d6138
spradere <spradere@pm.me>
2026-03-13 22:14:16 +0100

    move frequencies to board_define.h

--- src/hal/mcu/atmega2560/hal_i2c.h
+++ src/hal/mcu/atmega2560/hal_i2c.h
@@ -26,7 +26,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define I2C_FREQ 100000UL // Standard mode 100 kHz
+
 #define I2C_READ 1
 #define I2C_WRITE 0
 #define I2C_ACK 1
--- src/services/msg.c
+++ src/services/msg.c
@@ -69,7 +69,7 @@ void msg(void)
 
 	if( msgRequestChannel(&chan) == ERR_NO_ERROR )
 	{
-		msgWritreText(chan, "\3test hal_context 01", MSG_TO_LCD);
+		msgWritreText(chan, "\3move freq -> board", MSG_TO_LCD);
 	}
 
 	msgProcess();
