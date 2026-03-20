aa6135469cc4faeb5d6b3330d9bfda31c80b4435
Charlie Root <root@Z600>
2026-03-20 20:19:42 +0100

    move headers to hal/public timerSTC

--- src/hal/public/hal_timerSTC.h
+++ src/hal/public/hal_timerSTC.h
@@ -11,16 +11,16 @@
  * Powered by TaskMate, (c) 2026 PRADERE Sebastien
  */
 
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// ! Auto generated code, do not edit !
-// ! any changes will be lost         !
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#ifndef HAL_TIMERSTC_H
+#define HAL_TIMERSTC_H
 
-#ifndef AUTO_HAL_SYSTEM_H
-#define AUTO_HAL_SYSTEM_H
+#define HAL_TIMERSTC 0
 
-// clang-format off
-#include "hal/mcu/atmega2560/hal_timerSched.h"
-// clang-format on
+#if defined(ARCH_avr8) && defined(MCU_atmega2560) && defined(BOARD_arduinoMega)
+	#include "hal/mcu/atmega2560/hal_timerSTC_impl.h"
+	#undef HAL_TIMERSTC
+	#define HAL_TIMERSTC 1
+#endif
 
+_Static_assert(HAL_TIMERSTC, "No hal implementation for Software Time Counter on selected hardware target.");
 #endif
