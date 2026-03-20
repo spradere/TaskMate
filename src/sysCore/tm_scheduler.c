f87f0e2af2ae7138125b734d600a97e531512c6c
spradere <spradere@pm.me>
2026-03-20 20:32:53 +0100

    move headers to hal/public timerSched

--- src/sysCore/tm_scheduler.c
+++ src/sysCore/tm_scheduler.c
@@ -26,7 +26,7 @@
 #include "hal/auto_hal_system.h"
 #include "hal/public/hal_context.h"
 #include "hal/public/hal_stack.h"
-//#include "hal/auto_hal_user.h"
+#include "hal/public/hal_timerSched.h"
 #include "sysCall/sysCall.h"
 #include "sysCore/modules.h"
 
