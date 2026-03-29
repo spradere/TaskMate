c45ec9123e90d8f6817c75b047a4d33b584d97fc
spradere <spradere@pm.me>
2026-03-29 17:39:38 +0200

    fix arch/deps inversions

--- src/autoCode/globalError.c
+++ src/autoCode/globalError.c
@@ -54,8 +54,6 @@ void globalError(const char *src_name, error_catalog_t *errors, const char *dest
 	fprintf(file_tmp.stream, "#ifndef %s\n", guard_name);
 	fprintf(file_tmp.stream, "#define %s\n\n", guard_name);
 
-	fprintf(file_tmp.stream, "#include \"tm_libc/tm_string.h\"\n\n");
-
 	fprintf(file_tmp.stream, "typedef enum\n");
 	fprintf(file_tmp.stream, "{\n");
 	fprintf(file_tmp.stream, "\tERROR_LOW,\n");
@@ -135,11 +133,11 @@ void globalError(const char *src_name, error_catalog_t *errors, const char *dest
 	fprintf(file_tmp.stream, "\tERROR_COUNT\n");
 	fprintf(file_tmp.stream, "} err_codes_t;\n\n");
 
-	fprintf(file_tmp.stream, "typedef struct\n");
+	/*fprintf(file_tmp.stream, "typedef struct\n");
 	fprintf(file_tmp.stream, "{\n");
 	fprintf(file_tmp.stream, "\tconst tm_string_t *name;\n");
 	fprintf(file_tmp.stream, "\tconst err_critical_t critical;\n");
-	fprintf(file_tmp.stream, "} err_item_t;\n\n");
+	fprintf(file_tmp.stream, "} err_item_t;\n\n");*/
 
 	// end
 
--- src/hal/arch/avr8/hal_context_impl.c
+++ src/hal/arch/avr8/hal_context_impl.c
@@ -23,7 +23,7 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-#include "sysCore/modules_define.h"
+#include "interfaces/modules_define.h"
 #include "tm_libc/tm_syslog.h"
 
 // NOLINTBEGIN
