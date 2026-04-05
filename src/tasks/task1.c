9f00a26e22eacb6687993e8519a32f1ea95df552
spradere <spradere@pm.me>
2026-04-05 12:52:20 +0200

    check LICENSE update + format

--- src/tasks/task1.c
+++ src/tasks/task1.c
@@ -1,3 +1,11 @@
+/*
+ * TaskMate Project
+ * (c) 2026 PRADERE Sebastien
+ *
+ * This file is part of TaskMate and is distributed under the BSD 2-Clause License.
+ * See the LICENSE file for full license terms.
+ */
+
 /**
  * @file task1.c
  * @brief tasks task1 implementation.
