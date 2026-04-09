c46c6931dcaeed7869b261a447d81035d0b15446
spradere <spradere@pm.me>
2026-04-09 21:17:17 +0200

    update howto_git

--- src/hal/public/hal_sysInfo.c
+++ src/hal/public/hal_sysInfo.c
@@ -24,7 +24,7 @@
  */
 
 TM_STR_ROM_NEW(tm_ver, "0.27");
-const uint16_t tm_build = 4815;
+const uint16_t tm_build = 4817;
 TM_STR_ROM_NEW(arch_name, "avr8");
 TM_STR_ROM_NEW(mcu_name, "atmega2560");
 TM_STR_ROM_NEW(board_name, "arduinoMega");
