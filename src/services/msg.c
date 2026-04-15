3322ed81bb987ecaa8a0068e5b2670db89dfe979
spradere <spradere@pm.me>
2026-04-15 21:48:48 +0200

    move awk code from makefile to scripts

--- src/services/msg.c
+++ src/services/msg.c
@@ -64,7 +64,7 @@ void msg(void)
 
 	if( msgRequestChannel(&chan) == ERR_NO_ERROR )
 	{
-		msgWriteText(chan, "\3count loc 00 ", MSG_TO_LCD);
+		msgWriteText(chan, "\3awk build 00 ", MSG_TO_LCD);
 	}
 
 	msgProcess();
