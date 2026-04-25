bd6b5d2220fef58b285593cb3c6234c3637b5d7e
spradere <spradere@pm.me>
2026-04-25 18:52:37 +0200

    add prefix to makefiles hal_*

--- src/hal/mcu/atmega2560/hal_mcu_make.mk
+++ src/hal/mcu/atmega2560/hal_mcu_make.mk
@@ -8,11 +8,11 @@
 #
 ################################################################################
 
-# MCU settings
+# VAL_MCU_SERIAL settings
 
-MCU = atmega2560
-CFLAGS += -mmcu=${MCU}
+VAL_MCU_SERIAL = atmega2560
+CFLAGS += -mmcu=${VAL_MCU_SERIAL}
 
 # atmega2560 flash is 128 kB, PCHH is not implemented in context switch so flash is limited to 64 kB
-FLASH_SIZE_K=64
-RAM_SIZE_K=8
+VAL_FLASH_SIZE_K=64
+VAL_RAM_SIZE_K=8
