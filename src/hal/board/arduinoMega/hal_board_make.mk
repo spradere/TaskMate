f783adff504349f27c5efb0416f38446d8bbebec
spradere <spradere@pm.me>
2026-04-30 20:23:28 +0200

    remove arch/mcu/board from autoCode and makefiles, step 1

--- src/hal/board/arduinoMega/hal_board_make.mk
+++ src/hal/board/arduinoMega/hal_board_make.mk
@@ -11,6 +11,7 @@
 # Registration in the global system
 VAL_HW_STACK += arduinoMega
 PATH_SOURCES_LIST += ${PATH_SOURCES}/hal/board/arduinoMega
+CFLAGS += -DHW_BOARD_arduinoMega
 
 .include "${PATH_SOURCES}/hal/mcu/atmega2560/hal_mcu_make.mk"
 
