4952c06086c8569d0b7de9ff8707394807886883
spradere <spradere@pm.me>
2026-05-08 20:48:27 +0200

    update doc

--- src/services/msg.c
+++ src/services/msg.c
@@ -64,7 +64,7 @@ void msg(void)
 
 	if( msgRequestChannel(&chan) == ERR_NO_ERROR )
 	{
-		msgWriteText(chan, "\3ref. parseTag 02", MSG_TO_LCD);
+		msgWriteText(chan, "\3func dispatcher ", MSG_TO_LCD);
 	}
 
 	msgProcess();
