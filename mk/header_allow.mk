ab0d7724e29932b8bd46dfa87106b2eaa0ecd8c2
spradere <spradere@pm.me>
2026-05-14 22:24:48 +0200

    split code system/user 1

--- mk/header_allow.mk
+++ mk/header_allow.mk
@@ -12,32 +12,32 @@
 # Allowed source access to system critical headers
 ################################################################################
 
-CFLAGS_${PATH_SOURCES}/sysCore/modules.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/modules.c = \
 	-DHAL_SYSTEM_CRITICAL_ALLOWED \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCore/tm_scheduler.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/tm_scheduler.c = \
 	-DHAL_SYSTEM_CRITICAL_ALLOWED \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCore/TaskMate.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/TaskMate.c = \
 	-DHAL_SYSTEM_CRITICAL_ALLOWED \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCore/boot.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/boot.c = \
 	-DHAL_SYSTEM_CRITICAL_ALLOWED \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCore/runLevel.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/runLevel.c = \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCore/tm_softwareTimeCounter.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/tm_softwareTimeCounter.c = \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCall/sysCall.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCall/sysCall.c = \
 	-DTM_SYSTEM_CRITICAL_ALLOWED
 
-CFLAGS_${PATH_SOURCES}/sysCore/gpio.c = \
+CFLAGS_${PATH_SRCS_SYSTEM}/sysCore/gpio.c = \
 	-DHAL_SYSTEM_CRITICAL_ALLOWED
 
 
@@ -46,7 +46,7 @@ _system_critical_check:
 	@printf "\n%sChecking forbidden system critical includes ...%s\n" \
 		"${COLOUR_TARGET_INFO}" "${COLOUR_RESET}"
 
-	@awk ${COLOURS_AWK} -v PATH_SOURCES=${PATH_SOURCES} -v h_check_log=${FILE_H_CHECK_LOG} \
+	@awk ${COLOURS_AWK} -v PATH_SOURCES=${PATH_SRCS_SYSTEM} -v h_check_log=${FILE_H_CHECK_LOG} \
 		-f ${PATH_SCRIPTS}/header_allow.awk "${FILE_H_ALLOW_CONF}"
 
 
--- src_system/hal/board/arduinoMega/hal_board_make.mk
+++ src_system/hal/board/arduinoMega/hal_board_make.mk
@@ -13,13 +13,13 @@ HAL_BOARD_MAKE_MK = 1
 
 # Registration in the global system
 VAL_HW_STACK += arduinoMega
-PATH_ARDUINOMEGA = ${PATH_SOURCES}/hal/board/arduinoMega
+PATH_ARDUINOMEGA = ${PATH_SRCS_SYSTEM}/hal/board/arduinoMega
 PATHS_SOURCES += ${PATH_ARDUINOMEGA}
 FILES_HALINIT += ${PATH_ARDUINOMEGA}/hal_boardInit.h
 FILES_HALDEFINE += ${PATH_ARDUINOMEGA}/hal_board_define.h
 CFLAGS += -DBOARD_arduinoMega
 
-.include "${PATH_SOURCES}/hal/mcu/atmega2560/hal_mcu_make.mk"
+.include "${PATH_SRCS_SYSTEM}/hal/mcu/atmega2560/hal_mcu_make.mk"
 
 # board settings
 VAL_CPU_FREQ = 16000000UL
