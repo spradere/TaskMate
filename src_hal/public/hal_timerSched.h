894385e0d36a1bfea84cdbc1b4cd96c9fceb161f
spradere <spradere@pm.me>
2026-05-15 22:20:56 +0200

    add new directories hal and user

---
+++ src_hal/public/hal_timerSched.h
@@ -0,0 +1,26 @@
+/*
+ * TaskMate Project
+ * (c) 2026 PRADERE Sebastien
+ *
+ * This file is part of TaskMate and is distributed under the BSD-2-Clause License.
+ * See the LICENSE file for full license terms.
+ */
+
+/**
+ * @file hal_timerSched.h
+ * @brief public hal_timerSched header declarations.
+ *
+ */
+
+#ifndef HAL_TIMERSCHED_H
+#define HAL_TIMERSCHED_H
+
+#if defined(MCU_atmega2560)
+	#include "mcu/atmega2560/hal_timerSched_impl.h"
+	#define HAL_TIMER_SCHED
+#endif
+
+#if !defined(HAL_TIMER_SCHED)
+	#error "No hal implementation for scheduler timer on selected hardware target."
+#endif
+#endif
