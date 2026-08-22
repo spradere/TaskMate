ebec35d0faa12b3e39b3177a9cd06aaa107f9522
spradere <spradere@pm.me>
2026-08-22 11:45:32 +0200

    add driver status

--- srcs/interfaces/drivers.h
+++ srcs/interfaces/drivers.h
@@ -25,6 +25,7 @@
 #define DRV_CTRL_SETBIT 6
 #define DRV_CTRL_CLEARBIT 7
 #define DRV_CTRL_GETBIT 8
+#define DRV_CTRL_GETSTATUS 9
 #define DRV_UNKNOW 99
 
 // driver status bits
@@ -34,6 +35,13 @@ typedef uint8_t hal_driver_status_t;
 #define DRV_RL_MASK 0x07
 #define DRV_BIT_INIT 3
 #define DRV_BIT_START 4
-#define DRV_BIT_DEAD 5
+#define DRV_BIT_ERROR 5
+#define DRV_BIT_DEAD 6
+
+// driver status
+#define DRV_STATE_OFF 1
+#define DRV_STATE_INITIALIZED 2
+#define DRV_STATE_RUNNING 3
+#define DRV_STATE_ERROR 4
 
 #endif // INTERFACES_DRIVERS_H
--- srcs/system/services/msg.c
+++ srcs/system/services/msg.c
@@ -63,7 +63,7 @@ void msg(void)
 
 	if( msgRequestChannel(&chan) == ERR_NO_ERROR )
 	{
-		msgWriteText(chan, "\3dyn RunLevel 00", MSG_TO_LCD);
+		msgWriteText(chan, "\3driver go wild", MSG_TO_LCD);
 	}
 
 	msgProcess();
