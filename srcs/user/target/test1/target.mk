3df2fcaacc7f29d7a048cbb8f2e4ad74633f4ade
spradere <spradere@pm.me>
2026-09-17 22:31:09 +0200

    fix new fine file selection in build

--- srcs/user/target/test1/target.mk
+++ srcs/user/target/test1/target.mk
@@ -21,7 +21,12 @@ VAL_HW_STACK = test1
 PATH_TEST1 = ${PATH_SRCS}/user/target/test1
 PATHS_SOURCE_SEARCH += ${PATH_TEST1}
 FILES_EXTRA_SRC += ${PATH_TEST1}/targetWireSignal.c
+PATHS_EXTRA_SRC += ${PATH_SRCS}/hal/arch/avr8
+PATHS_EXTRA_SRC += 	${PATH_SRCS}/tmLibc
+
+# to be removed
 FILES_HALDEFINE = ${PATH_TEST1}/define.h
+#
 
 FILE_GPIO_SIGNALS = ${PATH_TEST1}/signals.gpio
 .if !exists(${FILE_GPIO_SIGNALS})
